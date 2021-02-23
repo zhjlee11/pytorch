6792fac540374a4b2066c73c5f38a0279cd0c3df
Chen Lai <chenlai@fb.com>
2021-02-22 21:18:31 -0800

    [PyTorch][CI] Enable test_lite_interpreter_runtime unittest in CI (macos)
    ghstack-source-id: cd6017c96543e211e479c5e7ec74efc18c060b9e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52566

---
+++ .jenkins/pytorch/macos-lite-interpreter-build-test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# shellcheck disable=SC2034
+source "$(dirname "${BASH_SOURCE[0]}")/macos-common.sh"
+
+git submodule sync --recursive
+git submodule update --init --recursive
+export CMAKE_PREFIX_PATH=${WORKSPACE_DIR}/miniconda3/
+
+# Build PyTorch
+if [ -z "${IN_CI}" ]; then
+  export DEVELOPER_DIR=/Applications/Xcode9.app/Contents/Developer
+fi
+
+echo "BUILD_LITE_INTERPRETER: ${BUILD_LITE_INTERPRETER}"
+if [ "${BUILD_LITE_INTERPRETER}" == 1 ]; then
+    echo "Testing libtorch (lite interpreter)."
+
+    CPP_BUILD="$PWD/../cpp-build"
+    rm -rf $CPP_BUILD
+    mkdir -p $CPP_BUILD/caffe2
+
+    BUILD_LIBTORCH_PY=$PWD/tools/build_libtorch.py
+    pushd $CPP_BUILD/caffe2
+    VERBOSE=1 DEBUG=1 python $BUILD_LIBTORCH_PY
+    popd
+
+    python tools/download_mnist.py --quiet -d test/cpp/api/mnist
+
+    # Unfortunately it seems like the test can't load from miniconda3
+    # without these paths being set
+    export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$PWD/miniconda3/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/miniconda3/lib"
+    echo "Finding test_lite_interpreter_runtime"
+    find . -regex '.*test_lite_interpreter_runtime'
+    pwd
+
+    assert_git_not_dirty
+else
+    echo "Skipping libtorch (lite interpreter)."
+fi
